689ff118902fd781771918e2ad3013346b82eb0c
niko <nick.connell@qanw.co.uk>
2021-02-24 23:04:53 +0000

    Displaying Album data on front end now

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -9,7 +9,47 @@
         <div class="row">
             <div class="col-md-12">
                 <h1>Albums</h1>
-                <h4>QANW Tech Test</h4>
+
+                <p>Number of albums: {{ $albumCount }}</p>
+
+                <div class="container bg-white shadow">
+                    <div class="row">
+                        <table class="table table-strped">
+                            <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">Album ID</th>
+                                <th scope="col">User ID</th>
+                                <th scope="col">Title</th>
+                            </tr>
+                            </thead>
+                            {{-- ToDo: Validation and error handling --}}
+                            @if ($albums)
+                                @foreach ($albums as $album)
+                                    <tr>
+                                        <td>
+                                            {{ $album->album_id }}
+                                        </td>
+                                        <td>
+                                            {{ $album->user_id }}
+                                        </td>
+                                        <td>
+                                            {{ $album->title }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td class="centeredTitleText" colspan="9">
+                                        No Albums Found
+                                    </td>
+                                </tr>
+                            @endif
+                        </table>
+                    </div>
+{{--                    <a href="{{route('album.create')}}" class="button noMarginBottom">--}}
+{{--                        Add New Album--}}
+{{--                    </a>--}}
+                </div>
             </div>
         </div>
     </div>
